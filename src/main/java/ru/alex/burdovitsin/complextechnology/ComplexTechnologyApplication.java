4057e4960cfac31f77167d6784b797d6ca0d3927
Alexander Burdovitsin <alex4rf@gmail.com>
2025-04-03 13:37:47 +0700

    Project init

---
+++ src/main/java/ru/alex/burdovitsin/complextechnology/ComplexTechnologyApplication.java
@@ -0,0 +1,13 @@
+package ru.alex.burdovitsin.complextechnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexTechnologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplexTechnologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alex/burdovitsin/complextechnology/ComplexTechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alex.burdovitsin.complextechnology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplexTechnologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
